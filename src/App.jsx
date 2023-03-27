84b2b10424b60302078bcbbbb63a751edd09be5d
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-03-27 11:57:21 -0300

    Ex.3-Criar useState para armazenar lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { useState } from 'react'
 import './App.css'
 import './Components/CardAdicionar'
 
 function App() {
-  
+  const [listaDeTarefas, setListaDeTarefas] = useState([]);
   return (
     <div className="App">
       <p>Criação do projeto</p>   
